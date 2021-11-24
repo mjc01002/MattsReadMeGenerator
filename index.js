304c4482e5e453d2f97b67887a531ce25651bb1a
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-11-24 11:04:10 -0500

    added questions

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//fs.writeFile('index.html', generatePage(name, github), err => {
+  //if (err) throw err;
+
+//  console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+
+console.log(inquirer);
